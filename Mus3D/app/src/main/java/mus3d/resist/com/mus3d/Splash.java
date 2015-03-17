b0044ed59402a25654775d0ea7acecff7e26840e
ArmindoMaurits <a.r.maurits@gmail.com>
2015-03-17 12:40:23 +0100

    Splashscreen gemaakt, nog met temp plaatje. Animaties toegevoegd

---
+++ Mus3D/app/src/main/java/mus3d/resist/com/mus3d/Splash.java
@@ -0,0 +1,23 @@
+package mus3d.resist.com.mus3d;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        overridePendingTransition(R.anim.anim_in, R.anim.anim_out);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
