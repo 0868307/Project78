dc2b7f5c224df692d86a90f4346166c3ede27a45
Armindo Maurits <0882940@hr.nl>
2015-03-18 20:46:56 +0100

    Toon kaart, deze download de data zelf. Moet nog een zelfgemaakte kaart in.

--- Mus3D/app/src/main/java/mus3d/resist/com/mus3d/map.java
+++ Mus3D/app/src/main/java/mus3d/resist/com/mus3d/map.java
@@ -1,52 +1,82 @@
 package mus3d.resist.com.mus3d;
 
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.OverlayItem;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
-import org.osmdroid.views.overlay.SimpleLocationOverlay;
+
+import java.util.ArrayList;
 
 
 public class map extends ActionBarActivity {
     private MapView mapView;
     private MapController mapController;
-    private ScaleBarOverlay mScaleBarOverlay;
-    private SimpleLocationOverlay mMyLocationOverlay;
+    private LocationManager locationManager;
+    private ArrayList<OverlayItem> overlayItemArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
         mapView = (MapView) this.findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
         mapView.setMultiTouchControls(true);
         mapController = (MapController) this.mapView.getController();
-        mapController.setZoom(2);
+        mapController.setZoom(12);
+
+        overlayItemArray = new ArrayList<OverlayItem>();
+
+        locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if(lastLocation != null){
+            updateLoc(lastLocation);
+        }
+
+        ScaleBarOverlay myScaleBarOverlay = new ScaleBarOverlay(this);
+        mapView.getOverlays().add(myScaleBarOverlay);
+    }
+
+    private void updateLoc(Location loc){
+        GeoPoint locGeoPoint = new GeoPoint(loc.getLatitude(), loc.getLongitude());
+        mapController.setCenter(locGeoPoint);
+
+        setOverlayLoc(loc);
+
+        mapView.invalidate();
+    }
+
+    private void setOverlayLoc(Location overlayloc){
+        GeoPoint overlocGeoPoint = new GeoPoint(overlayloc);
+        overlayItemArray.clear();
+        OverlayItem newMyLocationItem = new OverlayItem("My Location", "My Location", overlocGeoPoint);
+        overlayItemArray.add(newMyLocationItem);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_map, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
