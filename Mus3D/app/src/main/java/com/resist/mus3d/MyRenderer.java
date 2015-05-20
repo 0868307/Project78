4297653e84713d09a757b172748955b2045b8958
Thomas Maurer <0876337@hr.nl>
2015-05-20 14:31:14 +0200

    Poging tot progressbar in 3D. Laat nog niks zien.

--- Mus3D/app/src/main/java/com/resist/mus3d/MyRenderer.java
+++ Mus3D/app/src/main/java/com/resist/mus3d/MyRenderer.java
@@ -4,6 +4,9 @@ import android.content.Context;
 import android.graphics.Color;
 import android.opengl.GLES20;
 import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+
 import com.resist.mus3d.database.ObjectTable;
 import com.resist.mus3d.objects.Object;
 import com.resist.mus3d.objects.coords.Point;
@@ -36,48 +39,23 @@ public class MyRenderer extends RajawaliRenderer implements OnObjectPickedListen
     }
 
     public void initScene() {
+
+        stopRendering();
+        startGettingGPS();
+        mCamera.setLookAt(0, 0, 0);
+
+        // Create the default light
+        letThereBeLight();
+
+        }
+
+    private void letThereBeLight(){
         mLight = new DirectionalLight(1f, 0.2f, -1.0f); // set the direction
         mLight.setColor(1.0f, 1.0f, 1.0f);
         mLight.setPower(2);
-        mCamera.setLookAt(0, 0, 0);
-
-        ObjParser parser = new ObjParser(mContext.getResources(), mTextureManager, R.raw.bolder_obj);
-
-        try {
-            parser.parse();
-        } catch (AParser.ParsingException e) {
-            e.printStackTrace();
-        }
-        BaseObject3D mObject = parser.getParsedObject();
-        addChild(mObject);
-        mCamera.setZ(4.2f);
-        mCamera.setY(-1.5f);
-        mObject.setRotation(40, 0, 90);
-        mObject.setScale(.3f);
-        mObject.setDrawingMode(GLES20.GL_LINE_STRIP);
-        //setCamera(0,0,0);
-        mPicker = new ObjectColorPicker(this);
-        mPicker.setOnObjectPickedListener(this);
-        mPicker.registerObject(mObject);
-        System.out.println("Dus, ja"+mPicker);
-
-
-        /** ToonMaterial toonMat = new ToonMaterial();
-        toonMat.setToonColors(0xffffffff, 0xff000000, 0xff666666, 0xff000000);
-        mObject.setMaterial(toonMat);
-        mObject.addLight(mLight);
-
-        // -- Create an instance
-        OrthographicCamera camera = new OrthographicCamera();
-        // -- Set the zoom level
-        camera.setZoom(1); // -- this is the default
-        camera.setZoom(2);
-        // -- Set the look at coordinates
-        camera.setLookAt(1, 10, 3);
-        mObject.setScale(0.2f);
-         */
 
     }
+
     public void setCamera(float x, float y, float z){
         mCamera.setPosition(x, y, z);
     }
@@ -92,6 +70,21 @@ public class MyRenderer extends RajawaliRenderer implements OnObjectPickedListen
         }
         System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-");
     }
+
+    public void startGettingGPS(){
+        mHandler.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+
+                        new GetGPS().execute();
+
+                    }
+                }
+        );
+    }
+
+
     public void makeObjects(){
         new LongOperation().execute();
     }
@@ -100,9 +93,6 @@ public class MyRenderer extends RajawaliRenderer implements OnObjectPickedListen
     public void onObjectPicked(BaseObject3D object) {
         selectedObject = object;
         Object mijnObject = object3Ds.get(object);
-
-
-
     }
 
     public void getObjectAt(float x, float y) {
@@ -124,6 +114,95 @@ public class MyRenderer extends RajawaliRenderer implements OnObjectPickedListen
 
 
         return characterizedArray;
+    }
+
+
+    private void clearProgressLabel(){
+        Message msg = new Message();
+        msg.arg1 = 0;
+        mHandler.sendMessage(msg);
+    }
+
+    private void updateProgressLabel(String text, boolean determinate){
+        Message msg = new Message();
+        msg.arg1 = determinate ? 1 : 2;
+        msg.obj = text;
+        System.out.println(msg.arg1);
+        mHandler.sendMessage(msg);
+    }
+
+    private void setProgressLimit(int max)
+    {
+        Message msg = new Message();
+        msg.arg1 = 3;
+        msg.obj = max;
+        mHandler.sendMessage(msg);
+    }
+
+     private void resetProgress()
+    {
+        Message msg = new Message();
+        msg.arg1 = 3;
+        mHandler.sendMessage(msg);
+    }
+
+    private Handler mHandler = new Handler();
+
+    public void handleMessage(Message msg){
+        Rajawali act = (Rajawali)mContext;
+        switch (msg.arg1){
+            case 0:
+                act.clearLabel();
+                break;
+            case 1:
+                act.setLabel((String)msg.obj);
+                act.showProgressBar(true);
+                break;
+            case 2:
+                act.setLabel((String)msg.obj);
+                act.showProgressBar(false);
+                break;
+            case 3:
+                act.setProgressBarPosition(0);
+                break;
+
+
+        }
+    }
+
+    private class GetGPS
+            extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected Void doInBackground(Void... unused){
+
+            updateProgressLabel("GPS Locatie wordt bepaald...", false);
+
+            while (doIhaveALocation()) {
+                refreshGeometry();
+            }
+
+            return null;
+        }
+    }
+
+    private boolean doIhaveALocation(){
+        System.out.println("Trying to get GPS");
+        System.out.println(context.getLocation());
+        return context.getLocation() != null;
+    }
+
+
+    private void refreshGeometry(){
+        //resetProgress();
+
+        mSurfaceView.queueEvent(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        clearProgressLabel();
+                        startRendering();
+                    }
+                });
     }
 
     private class LongOperation extends AsyncTask<String, Void, Boolean> {
@@ -181,7 +260,6 @@ public class MyRenderer extends RajawaliRenderer implements OnObjectPickedListen
 
         @Override
         protected void onProgressUpdate(Void... values) {}
-
 
     }
 }
--- Mus3D/app/src/main/java/com/resist/mus3d/Rajawali.java
+++ Mus3D/app/src/main/java/com/resist/mus3d/Rajawali.java
@@ -1,5 +1,6 @@
 package com.resist.mus3d;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.location.Location;
 import android.os.Bundle;
@@ -8,7 +9,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.resist.mus3d.map.GpsActivity;
@@ -17,6 +21,8 @@ import com.resist.mus3d.map.Map;
 import com.resist.mus3d.sensor.SensorActivity;
 import com.resist.mus3d.sensor.SensorTracker;
 
+import java.util.logging.Handler;
+
 import rajawali.RajawaliActivity;
 
 public class Rajawali extends RajawaliActivity implements GpsActivity, SensorActivity, View.OnTouchListener {
@@ -24,32 +30,99 @@ public class Rajawali extends RajawaliActivity implements GpsActivity, SensorAct
     private LocationTracker locationListener;
     private SensorTracker sensorTracker;
     private int counter = 1000;
+    private ProgressBar mProgBarD;
+    private ProgressBar mProgBarI;
+    private TextView mLabel;
+    private Spinner mSpinner;
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         myRenderer = new MyRenderer(this);
         myRenderer.setSurfaceView(mSurfaceView);
         mSurfaceView.setOnTouchListener(this);
-
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        ll.setGravity(Gravity.CENTER);
-
-        TextView label = new TextView(this);
-        label.setText("Hola");
-        label.setTextSize(20);
-        label.setGravity(Gravity.CENTER);
-
-        label.setHeight(100);
-        ll.addView(label);
-
-        mLayout.addView(ll);
-
+        createUI();
 
         super.setRenderer(myRenderer);
         locationListener = new LocationTracker(this);
         sensorTracker = new SensorTracker(this);
     }
+
+    private void createUI(){
+        // The label
+        LinearLayout ll = new LinearLayout(this);
+        ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setGravity(Gravity.CENTER_HORIZONTAL + Gravity.CENTER_VERTICAL);
+
+        mLabel = new TextView(this);
+        mLabel.setGravity(Gravity.CENTER_HORIZONTAL);
+        mLabel.setTextSize(40);
+        mLabel.setPadding(0,0,0,30);
+        ll.addView(mLabel);
+
+        // The inderterminate progressbar
+        mProgBarI = new ProgressBar(this,null, R.id.progress_circular);
+        mProgBarI.setIndeterminate(true);
+        mProgBarI.setVisibility(View.GONE);
+        ll.addView(mProgBarI,50,50);
+
+        // the determinate progressbar
+        mProgBarD = new ProgressBar(this, null, R.id.progress_horizontal);
+        mProgBarD.setIndeterminate(false);
+        mProgBarD.setVisibility(View.GONE);
+        mProgBarD.setPadding(50,0,50,0);
+        mProgBarD.setProgress(0);
+        ll.addView(mProgBarD);
+
+        mLayout.addView(ll);
+
+        // Horizontal layout
+        LinearLayout bottom = new LinearLayout(this);
+        bottom.setOrientation(LinearLayout.HORIZONTAL);
+        bottom.setGravity(Gravity.BOTTOM);
+        bottom.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+
+        // Vertical layout
+        LinearLayout right = new LinearLayout(this);
+        right.setOrientation(LinearLayout.VERTICAL);
+        right.setGravity(Gravity.RIGHT);
+        right.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, 100));
+
+        bottom.addView(right);
+
+
+        mLayout.addView(bottom);
+    }
+
+    public void clearLabel(){
+        mLabel.setText("");
+        mProgBarI.setVisibility(View.GONE);
+        mProgBarD.setVisibility(View.GONE);
+        mLabel.setVisibility(View.GONE);
+        mSpinner.setVisibility(View.VISIBLE);
+    }
+
+    public void setLabel(String text){
+        mLabel.setVisibility(View.VISIBLE);
+        mLabel.setText(text);
+        mSpinner.setVisibility(View.GONE);
+    }
+
+    public void showProgressBar(boolean determinate){
+        int det = determinate ? View.VISIBLE : View.GONE,
+                ind = determinate ? View.GONE : View.VISIBLE;
+
+        mProgBarI.setVisibility(ind);
+        mProgBarD.setVisibility(det);
+    }
+
+    public void setProgressBarPosition(int i)
+    {
+        mProgBarD.setProgress(i);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
