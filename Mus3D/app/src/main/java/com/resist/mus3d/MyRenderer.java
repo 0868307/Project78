d55d2940d1766e30d0fddf8e64a2e28a09ec4155
Assumeru <malusluminis@hotmail.com>
2015-05-26 11:28:43 +0200

    laadbalkjes

--- Mus3D/app/src/main/java/com/resist/mus3d/MyRenderer.java
+++ Mus3D/app/src/main/java/com/resist/mus3d/MyRenderer.java
@@ -4,9 +4,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.opengl.GLES20;
 import android.os.AsyncTask;
-import android.os.Handler;
-import android.os.Message;
-
 import com.resist.mus3d.database.ObjectTable;
 import com.resist.mus3d.objects.Object;
 import com.resist.mus3d.objects.coords.Point;
@@ -23,243 +20,154 @@ import rajawali.util.ObjectColorPicker;
 import rajawali.util.OnObjectPickedListener;
 
 public class MyRenderer extends RajawaliRenderer implements OnObjectPickedListener {
-    public static final int MULTIPLIER = 10000;
-    private DirectionalLight mLight;
+	public static final int MULTIPLIER = 10000;
+	private DirectionalLight mLight;
 
-    private Map<BaseObject3D, Object> object3Ds = new HashMap<>();
-    private BaseObject3D selectedObject;
-    private Rajawali context;
-    private ObjectColorPicker mPicker;
-    private OnObjectPickedListener mObjectPickedListener;
-    public MyRenderer(Context context) {
-        super(context);
-        this.context = (Rajawali)context;
-        setFrameRate(60);
-        setBackgroundColor(Color.LTGRAY);
-    }
+	private Map<BaseObject3D, Object> object3Ds = new HashMap<>();
+	private BaseObject3D selectedObject;
+	private Rajawali context;
+	private ObjectColorPicker mPicker;
+	private OnObjectPickedListener mObjectPickedListener;
+	public MyRenderer(Rajawali context) {
+		super(context);
+		this.context = context;
+		setFrameRate(60);
+		setBackgroundColor(Color.LTGRAY);
+	}
 
-    public void initScene() {
+	public void initScene() {
+		mLight = new DirectionalLight(1f, 0.2f, -1.0f); // set the direction
+		mLight.setColor(1.0f, 1.0f, 1.0f);
+		mLight.setPower(2);
+		mCamera.setLookAt(0, 0, 0);
 
-        stopRendering();
-        startGettingGPS();
-        mCamera.setLookAt(0, 0, 0);
+		ObjParser parser = new ObjParser(mContext.getResources(), mTextureManager, R.raw.bolder_obj);
 
-        // Create the default light
-        letThereBeLight();
+		try {
+			parser.parse();
+		} catch (AParser.ParsingException e) {
+			e.printStackTrace();
+		}
+		BaseObject3D mObject = parser.getParsedObject();
+		addChild(mObject);
+		mCamera.setZ(4.2f);
+		mCamera.setY(-1.5f);
+		mObject.setRotation(40, 0, 90);
+		mObject.setScale(.3f);
+		mObject.setDrawingMode(GLES20.GL_LINE_STRIP);
+		//setCamera(0,0,0);
+		mPicker = new ObjectColorPicker(this);
+		mPicker.setOnObjectPickedListener(this);
+		mPicker.registerObject(mObject);
+		System.out.println("Dus, ja"+mPicker);
+	}
+	public void setCamera(float x, float y, float z){
+		mCamera.setPosition(x, y, z);
+	}
+	public void setCameraRotation(float x, float y, float z){
+		mCamera.setRotation(x, y, z);}
 
-        }
+	public void drawValues(final String... values){
+		System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-");
+		for (int i = 0; i < values.length; i++) {
+			System.out.println("value "+i+" : "+values[i]);
+			System.out.println("...........................");
+		}
+		System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-");
+	}
+	public void makeObjects(){
+		new LongOperation().execute();
+	}
 
-    private void letThereBeLight(){
-        mLight = new DirectionalLight(1f, 0.2f, -1.0f); // set the direction
-        mLight.setColor(1.0f, 1.0f, 1.0f);
-        mLight.setPower(2);
-
-    }
-
-    public void setCamera(float x, float y, float z){
-        mCamera.setPosition(x, y, z);
-    }
-    public void setCameraRotation(float x, float y, float z){
-        mCamera.setRotation(x, y, z);}
-
-    public void drawValues(final String... values){
-        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-");
-        for (int i = 0; i < values.length; i++) {
-            System.out.println("value "+i+" : "+values[i]);
-            System.out.println("...........................");
-        }
-        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-");
-    }
-
-    public void startGettingGPS(){
-        mHandler.post(
-                new Runnable() {
-                    @Override
-                    public void run() {
-
-                        new GetGPS().execute();
-
-                    }
-                }
-        );
-    }
-
-
-    public void makeObjects(){
-        new LongOperation().execute();
-    }
-
-    @Override
-    public void onObjectPicked(BaseObject3D object) {
-        selectedObject = object;
-        Object mijnObject = object3Ds.get(object);
-    }
-
-    public void getObjectAt(float x, float y) {
-        mPicker.getObjectAt(x, y);
-
-    }
-
-    public int[] getIntArray(Integer number){
-
-        int[] characterizedArray = new int[number.toString().length()];
-        String tussenweg;
-
-        for (int i = 0; i < number.toString().length() ; i++) {
-            char numberAsChar = number.toString().charAt(i);
-            tussenweg =""+numberAsChar;
-            int parsedInt = Integer.parseInt(tussenweg);
-            characterizedArray[i] = parsedInt;
-        }
-
-
-        return characterizedArray;
-    }
-
-
-    private void clearProgressLabel(){
-        Message msg = new Message();
-        msg.arg1 = 0;
-        mHandler.sendMessage(msg);
-    }
-
-    private void updateProgressLabel(String text, boolean determinate){
-        Message msg = new Message();
-        msg.arg1 = determinate ? 1 : 2;
-        msg.obj = text;
-        System.out.println(msg.arg1);
-        mHandler.sendMessage(msg);
-    }
-
-    private void setProgressLimit(int max)
-    {
-        Message msg = new Message();
-        msg.arg1 = 3;
-        msg.obj = max;
-        mHandler.sendMessage(msg);
-    }
-
-     private void resetProgress()
-    {
-        Message msg = new Message();
-        msg.arg1 = 3;
-        mHandler.sendMessage(msg);
-    }
-
-    private Handler mHandler = new Handler();
-
-    public void handleMessage(Message msg){
-        Rajawali act = (Rajawali)mContext;
-        switch (msg.arg1){
-            case 0:
-                act.clearLabel();
-                break;
-            case 1:
-                act.setLabel((String)msg.obj);
-                act.showProgressBar(true);
-                break;
-            case 2:
-                act.setLabel((String)msg.obj);
-                act.showProgressBar(false);
-                break;
-            case 3:
-                act.setProgressBarPosition(0);
-                break;
-
-
-        }
-    }
-
-    private class GetGPS
-            extends AsyncTask<Void,Void,Void>{
-        @Override
-        protected Void doInBackground(Void... unused){
-
-            updateProgressLabel("GPS Locatie wordt bepaald...", false);
-
-            while (doIhaveALocation()) {
-                refreshGeometry();
-            }
-
-            return null;
-        }
-    }
-
-    private boolean doIhaveALocation(){
-        System.out.println("Trying to get GPS");
-        System.out.println(context.getLocation());
-        return context.getLocation() != null;
-    }
-
-
-    private void refreshGeometry(){
-        //resetProgress();
-
-        mSurfaceView.queueEvent(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        clearProgressLabel();
-                        startRendering();
-                    }
-                });
-    }
-
-    private class LongOperation extends AsyncTask<String, Void, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(String... params) {
-            ObjectTable objectTable = new ObjectTable(Mus3D.getDatabase().getDatabase());
-            List<? extends com.resist.mus3d.objects.Object> list = objectTable.getObjectsAround(new Point(context.getLocation()), 0.001);
-            Map<BaseObject3D, Object> newobject3Ds = new HashMap<>();
-            Position lastPos = null;
-            System.out.println("lat = "+context.getLocation().getLatitude()*MULTIPLIER+" long = "+context.getLocation().getLongitude()*MULTIPLIER);
-            System.out.println("list size = "+list.size());
-            for (Object o : list) {
-                ObjParser parser = new ObjParser(mContext.getResources(), mTextureManager, R.raw.bolder_obj);
-
-                try {
-                    parser.parse();
-                } catch (AParser.ParsingException e) {
-                    e.printStackTrace();
-                }
-                BaseObject3D mObject = parser.getParsedObject();
-                addChild(mObject);
-                mPicker.registerObject(mObject);
-                System.out.println(mPicker);
-                lastPos = o.getLocation().getPosition();
-                mObject.setPosition((float) (lastPos.getLongitude()*MULTIPLIER) ,0, (float) (lastPos.getLatitude()*MULTIPLIER) );
-                mObject.setRotation(0, 0, 0);
-                mObject.setScale(.1f);
-                mObject.setDrawingMode(GLES20.GL_LINE_STRIP);
-                newobject3Ds.put(mObject,o);
-                System.out.println("++++++++++++++++++++++++++++++++++++");
-                System.out.println(lastPos.getLatitude()*MULTIPLIER);
-                System.out.println(lastPos.getLongitude()*MULTIPLIER);
-                System.out.println("||||||||||||||||||||||||||||||||||||");
-            }
-            System.out.println("executed");
-            for (Map.Entry<BaseObject3D, Object> o : object3Ds.entrySet()) {
-                removeChild(o.getKey());
-            }
-            object3Ds = newobject3Ds;
-            System.out.println("old objects removed");
-
-            return true;
-        }
+	@Override
+	public void onObjectPicked(BaseObject3D object) {
+		selectedObject = object;
+		Object mijnObject = object3Ds.get(object);
 
 
 
-        @Override
-        protected void onPostExecute(Boolean result) {
-        }
+	}
 
-        @Override
-        protected void onPreExecute() {}
+	public void getObjectAt(float x, float y) {
+		mPicker.getObjectAt(x, y);
+
+	}
+
+	public int[] getIntArray(Integer number){
+
+		int[] characterizedArray = new int[number.toString().length()];
+		String tussenweg;
+
+		for (int i = 0; i < number.toString().length() ; i++) {
+			char numberAsChar = number.toString().charAt(i);
+			tussenweg =""+numberAsChar;
+			int parsedInt = Integer.parseInt(tussenweg);
+			characterizedArray[i] = parsedInt;
+		}
 
 
-        @Override
-        protected void onProgressUpdate(Void... values) {}
+		return characterizedArray;
+	}
 
-    }
+	private class LongOperation extends AsyncTask<String, Void, Boolean> {
+
+		@Override
+		protected Boolean doInBackground(String... params) {
+			ObjectTable objectTable = new ObjectTable(Mus3D.getDatabase().getDatabase());
+			List<? extends com.resist.mus3d.objects.Object> list = objectTable.getObjectsAround(new Point(context.getLocation()), 0.001);
+			Map<BaseObject3D, Object> newobject3Ds = new HashMap<>();
+			Position lastPos = null;
+			System.out.println("lat = "+context.getLocation().getLatitude()*MULTIPLIER+" long = "+context.getLocation().getLongitude()*MULTIPLIER);
+			System.out.println("list size = "+list.size());
+			for (int n=0, size = list.size(); n < size; n++) {
+				Object o = list.get(n);
+				ObjParser parser = new ObjParser(mContext.getResources(), mTextureManager, R.raw.bolder_obj);
+
+				try {
+					parser.parse();
+				} catch (AParser.ParsingException e) {
+					e.printStackTrace();
+				}
+				BaseObject3D mObject = parser.getParsedObject();
+				addChild(mObject);
+				mPicker.registerObject(mObject);
+				System.out.println(mPicker);
+				lastPos = o.getLocation().getPosition();
+				mObject.setPosition((float) (lastPos.getLongitude()*MULTIPLIER) ,0, (float) (lastPos.getLatitude()*MULTIPLIER) );
+				mObject.setRotation(0, 0, 0);
+				mObject.setScale(.1f);
+				mObject.setDrawingMode(GLES20.GL_LINE_STRIP);
+				newobject3Ds.put(mObject,o);
+				System.out.println("++++++++++++++++++++++++++++++++++++");
+				System.out.println(lastPos.getLatitude()*MULTIPLIER);
+				System.out.println(lastPos.getLongitude()*MULTIPLIER);
+				System.out.println("||||||||||||||||||||||||||||||||||||");
+				context.onObjectLoadingProgress((double)n / (double)size * 100.0);
+			}
+			System.out.println("executed");
+			for (Map.Entry<BaseObject3D, Object> o : object3Ds.entrySet()) {
+				removeChild(o.getKey());
+			}
+			object3Ds = newobject3Ds;
+			System.out.println("old objects removed");
+
+			return true;
+		}
+
+
+
+		@Override
+		protected void onPostExecute(Boolean result) {
+			context.onObjectsLoaded(result);
+		}
+
+		@Override
+		protected void onPreExecute() {}
+
+
+		@Override
+		protected void onProgressUpdate(Void... values) {}
+
+
+	}
 }
--- Mus3D/app/src/main/java/com/resist/mus3d/Rajawali.java
+++ Mus3D/app/src/main/java/com/resist/mus3d/Rajawali.java
@@ -2,18 +2,21 @@ package com.resist.mus3d;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
-import android.widget.TextView;
+import android.widget.RelativeLayout;
 
 import com.resist.mus3d.map.GpsActivity;
 import com.resist.mus3d.map.LocationTracker;
@@ -21,191 +24,132 @@ import com.resist.mus3d.map.Map;
 import com.resist.mus3d.sensor.SensorActivity;
 import com.resist.mus3d.sensor.SensorTracker;
 
-import java.util.logging.Handler;
-
 import rajawali.RajawaliActivity;
 
 public class Rajawali extends RajawaliActivity implements GpsActivity, SensorActivity, View.OnTouchListener {
-    private MyRenderer myRenderer;
-    private LocationTracker locationListener;
-    private SensorTracker sensorTracker;
-    private int counter = 1000;
-    private ProgressBar mProgBarD;
-    private ProgressBar mProgBarI;
-    private TextView mLabel;
-    private Spinner mSpinner;
+	private MyRenderer myRenderer;
+	private LocationTracker locationListener;
+	private SensorTracker sensorTracker;
+	private int counter = 1000;
+	private View progressBarGPS;
+	private View progressBarObjects;
 
+	@Override public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		myRenderer = new MyRenderer(this);
+		myRenderer.setSurfaceView(mSurfaceView);
+		mSurfaceView.setOnTouchListener(this);
 
+		RelativeLayout rl = new RelativeLayout(this);
+		LinearLayout ll = new LinearLayout(this);
+		ll.setOrientation(LinearLayout.VERTICAL);
+		ll.setGravity(Gravity.CENTER);
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        myRenderer = new MyRenderer(this);
-        myRenderer.setSurfaceView(mSurfaceView);
-        mSurfaceView.setOnTouchListener(this);
-        createUI();
+		rl.addView(ll);
+		LayoutInflater inflater = LayoutInflater.from(this);
+		progressBarGPS = inflater.inflate(R.layout.progress_gps, null);
+		progressBarObjects = inflater.inflate(R.layout.progress_objects, null);
+		progressBarObjects.setVisibility(View.GONE);
+		rl.addView(progressBarGPS);
+		rl.addView(progressBarObjects);
+		mLayout.addView(rl);
 
-        super.setRenderer(myRenderer);
-        locationListener = new LocationTracker(this);
-        sensorTracker = new SensorTracker(this);
-    }
+		super.setRenderer(myRenderer);
+		locationListener = new LocationTracker(this);
+		sensorTracker = new SensorTracker(this);
+	}
 
-    private void createUI(){
-        // The label
-        LinearLayout ll = new LinearLayout(this);
-        ll.setOrientation(LinearLayout.VERTICAL);
-        ll.setGravity(Gravity.CENTER_HORIZONTAL + Gravity.CENTER_VERTICAL);
+	public void onObjectLoadingProgress(double progress) {
+		ProgressBar bar = (ProgressBar)((ViewGroup) progressBarObjects).getChildAt(0);
+		bar.setProgress((int)(progress*10));
+	}
 
-        mLabel = new TextView(this);
-        mLabel.setGravity(Gravity.CENTER_HORIZONTAL);
-        mLabel.setTextSize(40);
-        mLabel.setPadding(0,0,0,30);
-        ll.addView(mLabel);
+	public void onObjectsLoaded(boolean success) {
+		Log.d(Mus3D.LOG_TAG, "success: "+success);
+		if(success) {
+			progressBarObjects.setVisibility(View.GONE);
+		}
+	}
 
-        // The inderterminate progressbar
-        mProgBarI = new ProgressBar(this,null, R.id.progress_circular);
-        mProgBarI.setIndeterminate(true);
-        mProgBarI.setVisibility(View.GONE);
-        ll.addView(mProgBarI,50,50);
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.menu_settings3d, menu);
+		return true;
+	}
 
-        // the determinate progressbar
-        mProgBarD = new ProgressBar(this, null, R.id.progress_horizontal);
-        mProgBarD.setIndeterminate(false);
-        mProgBarD.setVisibility(View.GONE);
-        mProgBarD.setPadding(50,0,50,0);
-        mProgBarD.setProgress(0);
-        ll.addView(mProgBarD);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
 
-        mLayout.addView(ll);
+		switch(id) {
+			case R.id.settings_Settings:
+				Intent i = new Intent(this, Settings.class);
+				startActivity(i);
+				break;
+			case R.id.action_2D:
+				Intent j = new Intent(this, Map.class);
+				startActivity(j);
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	@Override
+	protected void onStop() {
+		locationListener.onStop();
+		sensorTracker.onStop();
+		super.onStop();
+	}
 
-        // Horizontal layout
-        LinearLayout bottom = new LinearLayout(this);
-        bottom.setOrientation(LinearLayout.HORIZONTAL);
-        bottom.setGravity(Gravity.BOTTOM);
-        bottom.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.FILL_PARENT));
+	@Override
+	protected void onResume() {
+		locationListener.onResume();
+		sensorTracker.onResume();
+		super.onResume();
+	}
+	@Override
+	public void update() {
+		if(locationListener != null) {
+			if(progressBarGPS.getVisibility() == View.VISIBLE) {
+				Log.d(Mus3D.LOG_TAG, "Loaded GPS");
+				progressBarGPS.setVisibility(View.GONE);
+				progressBarObjects.setVisibility(View.VISIBLE);
+			}
+			myRenderer.setCamera(
+					(float) locationListener.getCurrentLocation().getLongitude()*MyRenderer.MULTIPLIER,
+					0,
+					(float) locationListener.getCurrentLocation().getLatitude()*MyRenderer.MULTIPLIER
+			);
+			if(counter > 1000){
+				myRenderer.makeObjects();
+				counter = 0;
+			}
+		}
+		counter++;
+	}
+	public Location getLocation(){
+		if(locationListener != null && locationListener.getCurrentLocation() != null)
+			return locationListener.getCurrentLocation();
+		Location location = new Location((String)null);
+		location.setLatitude(0);
+		location.setLongitude(0);
+		return location;
+	}
 
-        // Vertical layout
-        LinearLayout right = new LinearLayout(this);
-        right.setOrientation(LinearLayout.VERTICAL);
-        right.setGravity(Gravity.RIGHT);
-        right.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, 100));
+	@Override
+	public void updateSensor(float x,float y,float z) {
+		if(sensorTracker != null){
+			myRenderer.setCameraRotation(x,-y,z);
+		}
+	}
 
-        bottom.addView(right);
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		if(event.getAction() == MotionEvent.ACTION_DOWN)
+		{
+			myRenderer.getObjectAt(event.getX(), event.getY());
 
+		}
 
-        mLayout.addView(bottom);
-    }
-
-    public void clearLabel(){
-        mLabel.setText("");
-        mProgBarI.setVisibility(View.GONE);
-        mProgBarD.setVisibility(View.GONE);
-        mLabel.setVisibility(View.GONE);
-        mSpinner.setVisibility(View.VISIBLE);
-    }
-
-    public void setLabel(String text){
-        mLabel.setVisibility(View.VISIBLE);
-        mLabel.setText(text);
-        mSpinner.setVisibility(View.GONE);
-    }
-
-    public void showProgressBar(boolean determinate){
-        int det = determinate ? View.VISIBLE : View.GONE,
-                ind = determinate ? View.GONE : View.VISIBLE;
-
-        mProgBarI.setVisibility(ind);
-        mProgBarD.setVisibility(det);
-    }
-
-    public void setProgressBarPosition(int i)
-    {
-        mProgBarD.setProgress(i);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_settings3d, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        switch(id) {
-            case R.id.settings_Settings:
-                Intent i = new Intent(this, Settings.class);
-                startActivity(i);
-                break;
-            case R.id.action_2D:
-                Intent j = new Intent(this, Map.class);
-                startActivity(j);
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    @Override
-    protected void onStop() {
-        locationListener.onStop();
-        sensorTracker.onStop();
-        super.onStop();
-    }
-
-    @Override
-    protected void onResume() {
-        locationListener.onResume();
-        sensorTracker.onResume();
-        super.onResume();
-    }
-    @Override
-    public void update() {
-        if(locationListener != null) {
-            /*System.out.println("");
-            System.out.println(
-                    "latitude : " + (float) locationListener.getCurrentLocation().getLatitude() +
-                            "\n longtitude : " + (float) locationListener.getCurrentLocation().getLongitude() +
-                            "\n altitude : " + (float) locationListener.getCurrentLocation().getAltitude()+
-                    "\n angle : " +  locationListener.getLastDirection()
-            );
-            */
-            myRenderer.setCamera(
-                    (float) locationListener.getCurrentLocation().getLongitude()*MyRenderer.MULTIPLIER,
-                    0,
-                    (float) locationListener.getCurrentLocation().getLatitude()*MyRenderer.MULTIPLIER
-            );
-            if(counter > 1000){
-                myRenderer.makeObjects();
-                counter = 0;
-            }
-        }
-        counter++;
-    }
-    public Location getLocation(){
-        if(locationListener != null && locationListener.getCurrentLocation() != null)
-            return locationListener.getCurrentLocation();
-        Location location = new Location((String)null);
-        location.setLatitude(0);
-        location.setLongitude(0);
-        return location;
-    }
-
-    @Override
-    public void updateSensor(float x,float y,float z) {
-        if(sensorTracker != null){
-            myRenderer.setCameraRotation(x,-y,z);
-        }
-    }
-
-    @Override
-    public boolean onTouch(View v, MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN)
-        {
-            myRenderer.getObjectAt(event.getX(), event.getY());
-
-        }
-
-        return super.onTouchEvent(event);
-    }
+		return super.onTouchEvent(event);
+	}
 }
