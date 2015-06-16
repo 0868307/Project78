0ff9d1e954090438156b31a8a70f18c378c2fbca
0868307 <0868307@hr.nl>
2015-06-16 02:17:10 +0200

    minimap 3d

--- Mus3D/app/src/main/java/com/resist/mus3d/ar/Rajawali.java
+++ Mus3D/app/src/main/java/com/resist/mus3d/ar/Rajawali.java
@@ -25,6 +25,14 @@ import com.resist.mus3d.map.LocationTracker;
 import com.resist.mus3d.sensor.SensorActivity;
 import com.resist.mus3d.sensor.SensorTracker;
 
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
+
+import java.util.ArrayList;
+
 import rajawali.RajawaliActivity;
 
 public class Rajawali extends RajawaliActivity implements GpsActivity, SensorActivity, View.OnTouchListener {
@@ -34,7 +42,8 @@ public class Rajawali extends RajawaliActivity implements GpsActivity, SensorAct
 	private int counter = 1000;
 	private View progressBarGPS;
 	private View progressBarObjects;
-
+	private final int ZOOMLEVEL = 16;
+	private MapView minimap;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -48,14 +57,13 @@ public class Rajawali extends RajawaliActivity implements GpsActivity, SensorAct
 	}
 
 	private void createUI(){
-
 		RelativeLayout rl = new RelativeLayout(this);
 		LinearLayout ll = new LinearLayout(this);
 		ll.setOrientation(LinearLayout.VERTICAL);
 		ll.setGravity(Gravity.CENTER);
-
 		rl.addView(ll);
 		LayoutInflater inflater = LayoutInflater.from(this);
+		addMinimap(rl, inflater);
 		progressBarGPS = inflater.inflate(R.layout.progress_gps, null);
 		progressBarObjects = inflater.inflate(R.layout.progress_objects, null);
 		progressBarObjects.setVisibility(View.GONE);
@@ -63,6 +71,25 @@ public class Rajawali extends RajawaliActivity implements GpsActivity, SensorAct
 		rl.addView(progressBarObjects);
 		mLayout.addView(rl);
 
+	}
+	private void addMinimap(RelativeLayout layout, LayoutInflater inflater){
+		LinearLayout mapViewParent = (LinearLayout)inflater.inflate(R.layout.minimap,null);
+		minimap = (MapView)mapViewParent.getChildAt(0);
+		minimap.setMultiTouchControls(false);
+		minimap.setBuiltInZoomControls(false);
+		MapController mapController = (MapController) minimap.getController();
+		mapController.setZoom(ZOOMLEVEL);
+		addCurrentPosition();
+		layout.addView(mapViewParent);
+	}
+	private void addCurrentPosition() {
+		if (locationListener != null && locationListener.getCurrentLocation() != null) {
+			GeoPoint currentLocation = new GeoPoint(locationListener.getCurrentLocation().getLatitude(), locationListener.getCurrentLocation().getLongitude());
+			OverlayItem currentLoc = new OverlayItem("location", "Huidige location", currentLocation);
+			ItemizedIconOverlay<OverlayItem> itemizedIconOverlay = new ItemizedIconOverlay<>(this, new ArrayList<OverlayItem>(), null);
+			itemizedIconOverlay.addItem(currentLoc);
+			minimap.getOverlays().add(itemizedIconOverlay);
+		}
 	}
 
 	private void updateRotation(String string){
@@ -122,6 +149,8 @@ public class Rajawali extends RajawaliActivity implements GpsActivity, SensorAct
 					0,
 					(float) locationListener.getCurrentLocation().getLatitude()* ObjectRenderer.MULTIPLIER
 			);
+			GeoPoint currentLocation = new GeoPoint(locationListener.getCurrentLocation().getLatitude(), locationListener.getCurrentLocation().getLongitude());
+			minimap.getController().setCenter(currentLocation);
 			if(counter > 1000){
 				objectRenderer.makeObjects();
 				counter = 0;
@@ -141,8 +170,9 @@ public class Rajawali extends RajawaliActivity implements GpsActivity, SensorAct
 	@Override
 	public void updateSensor(float x,float y,float z) {
 		if(sensorTracker != null){
-			objectRenderer.setCameraRotation(x,-y,z);
-			updateRotation(""+(-y));
+			objectRenderer.setCameraRotation(x,y,z);
+			updateRotation("" + (y));
+			minimap.setRotation(((-y+180)%360));
 		}
 	}
 
